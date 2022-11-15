aa3cf8b149526d6369c4232b73099dafd798c601
ionadam97 <100874180+ionadam97@users.noreply.github.com>
2022-11-15 10:27:41 +0200

    Update csv_read.py

--- csv_read.py
+++ csv_read.py
@@ -6,14 +6,15 @@ while True:
         if a == 0:
             data = introducere_fisier()
             outlet, ct, data_luna = transformare_ora(data)
+            
         timp1 = introducere_ora(outlet, ct, data_luna)
         t1, t2 = cautare_sesiune(data, timp1)
         numar_joc(data, t1, t2)
         nume_joc(data, t1, t2)
         mizaj(data, t1, t2)
         b = int(input("\nIntroduceti 1 pentru a cauta in acest fisier"
-                      "\nIntroduceti 2 pentru a deschide un fisier nou\n"))
-        if b <= 1:
+                      "\nIntroduceti alt numar pentru a deschide un fisier nou\n"))
+        if b = 1:
             a = 1
         else:
             a = 0
